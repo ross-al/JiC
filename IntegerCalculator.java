a2cefe9b5b0afb5b26f6ede2d3c60419ec0ede2c
ross-al <115371010+ross-al@users.noreply.github.com>
2022-11-01 22:29:01 +0000

    successful javac and java

--- IntegerCalculator.java
+++ IntegerCalculator.java
@@ -5,11 +5,11 @@ public class IntegerCalculator {
 public static void main(String[] args) {
 
 
-String a = add(2,2);
-String b = substract(2,2);
-String c = multiply(2,2);
-String d = divide(2,2);
-String e = modulus(2,2);
+System.out.println("ADD: " + Calculator.add(2,2));
+System.out.println("SUBTRACT: " + Calculator.subtract(2,2));
+System.out.println("MULTIPLY: " + Calculator.multiply(2,2));
+System.out.println("DIVIDE: " + Calculator.divide(2,2));
+System.out.println("MODULUS: " + Calculator.modulus(2,2));
 
 //add class 
 
@@ -17,37 +17,37 @@ String e = modulus(2,2);
 
 class Calculator {
 
+//needs to be static as method belongs to class, not object.
+//i.e. there's no object to call from as we have not made an instance of this class anywhere
+//all methods of a class must be private, unless their purpose is to be called outside the class
+//i.e. like here where we call the methods outside of the class
+//if a class has methods (like this one), its fields must be private (there are no fields here fyi)
 
-int add(int x, int y){
+public static int add(int x, int y){ 
 int z = x + y;
 return z;
-System.out.println(z);
 }
 
-int subtract(int x, int y){
+public static int subtract(int x, int y){
 int z = x - y;
 return z;
-System.out.println(z);
 }
 
-int multiply(int x, int y){
+public static int multiply(int x, int y){
 int z = x * y;
 return z;
-System.out.println(z);
 }
 
-double divide(int a, int b){
+public static double divide(int a, int b){
 double x = (double) a;
 double y = (double) b;
 double z = x/y;
 return z;
-System.out.println(z);
 }
 
-int modulus(int x, int y){
+public static int modulus(int x, int y){
 int z = x % y;
 return z;
-System.out.println(z);
 }
 
 
