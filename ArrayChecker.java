8cd28550b4d1f6548e6ea9af46cc633aaf22d487
ross-al <115371010+ross-al@users.noreply.github.com>
2022-11-08 16:37:52 +0000

    first attempt at array methods

---
+++ ArrayChecker.java
@@ -0,0 +1,32 @@
+public class ArrayChecker{
+
+int[] myArray;
+
+public boolean isSymmetrical(int[] myArray){
+
+//first try a method to get the character at position x
+// myArray[myArray.length - 1]
+
+if (myArray[0] == myArray[myArray.length-1]){
+return true;
+	}
+else return false;
+}
+
+public String reverse(){
+
+}
+
+
+} //close ArrayChecker
+
+
+
+
+
+
+
+
+
+
+
---
+++ Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+
+public static void main(String[] args) {
+
+// Here be the code to launch the program
+//call ArrayChecker (make sure to compile both files)
+
+}
+
+}
