70d23fe43537f53f096829e85441c67a8fa85f60
Mr Ross Britten <rbritt05@dcs.bbk.ac.uk>
2022-11-08 21:01:20 +0000

    proof of concept works!

--- Arrays.java
+++ Arrays.java
@@ -1,25 +1,25 @@
+//can use launcher here instead
+//if so add code below outside of main method
+//create array variable
+//create new object of ArrayChecker class using
+//inputArray as param
+//call ArrayChecker methods (make sure to compile both files)
+
+
+
+
 public class Arrays {
 
 public static void main(String[] args) {
 
-//create array variable
-
-int[] inputArray = {123,55,55,123};
-
-//create new object of ArrayChecker class using
-//inputArray as param
-
-ArrayChecker myArrayChecker = new ArrayChecker(inputArray);
-
-//call ArrayChecker methods (make sure to compile both files)
-
-
-if (myArrayChecker.isSymmetrical(inputArray)) {
-System.out.println("Yes, it's symmetrical");
-}
-else {
-System.out.println(myArrayChecker.reverse());
-}
+ int[] inputArray = {123,55,55,123};
+ ArrayChecker myArrayChecker = new ArrayChecker();
+ if (myArrayChecker.isSymmetrical(inputArray)) {
+   System.out.println("Yes, it's symmetrical");
+ }
+ //else {
+   //System.out.println(myArrayChecker.reverse());
+ //}
 
 
 }
