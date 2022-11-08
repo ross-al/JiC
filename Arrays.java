c5a7e8d7e3319c3600122553b50bf55df3f76f1c
Mr Ross Britten <rbritt05@dcs.bbk.ac.uk>
2022-11-08 20:36:01 +0000

    adding saved file from class

--- Arrays.java
+++ Arrays.java
@@ -2,8 +2,25 @@ public class Arrays {
 
 public static void main(String[] args) {
 
-// Here be the code to launch the program
-//call ArrayChecker (make sure to compile both files)
+//create array variable
+
+int[] inputArray = {123,55,55,123};
+
+//create new object of ArrayChecker class using
+//inputArray as param
+
+ArrayChecker myArrayChecker = new ArrayChecker(inputArray);
+
+//call ArrayChecker methods (make sure to compile both files)
+
+
+if (myArrayChecker.isSymmetrical(inputArray)) {
+System.out.println("Yes, it's symmetrical");
+}
+else {
+System.out.println(myArrayChecker.reverse());
+}
+
 
 }
 
