1d4d37dcb8ad7602fe2650b20f3d1a35ad74f8ea
Mr Ross Britten <rbritt05@dcs.bbk.ac.uk>
2022-11-08 21:00:58 +0000

    proof of concept works!

--- ArrayChecker.java
+++ ArrayChecker.java
@@ -1,35 +1,34 @@
-public class ArrayChecker{
-
-//don't actually need a constructor here
+//don't actually need a constructor here, but we can do it this way
 //public constructor to create new instance of AC. 
 //private int[] x;
 //ArrayChecker(int[] x){
 //this.x = x;
 //}
 
-
-
 //first try a method to get the character at position x
 // myArray[myArray.length - 1]
-//should we use == with array or .equals?
+
 
 //public boolean isSymmetrical() {
   //if (this.x[0] == this.x[this.x.length-1]) {
  
 
-
+public class ArrayChecker{
 
 public boolean isSymmetrical(int [] x){
   if (x[0] == x[x.length-1]){
-return true;
-	}
-else return false;
+  return true;
+  }
+  else {
+  return false;
+  }
 }
 
-public String reverse(){
-int updatedString = 
-return ;
-}
+
+//public String reverse(){
+//int updatedString = 
+//return ;
+//}
 
 
 } //close ArrayChecker
