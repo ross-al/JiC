95773b3473dfeebdacd32d65ac67cfc2b7c8b636
Mr Ross Britten <rbritt05@dcs.bbk.ac.uk>
2022-12-06 21:02:56 +0000

    adding generic Stack class with generic constructor

---
+++ Stack.java
@@ -0,0 +1,42 @@
+public class Stack<T extends Number>{
+
+	//private int intValue;
+	//private double doubleValue;
+	
+	private T value; //create generic type member field? 
+	
+	public Stack(T value){
+		this.value = value;
+	}
+	
+
+	//public Stack(int intValue){
+		//this.intValue = intValue;
+	//}
+	
+	//public Stack(double doubleValue){
+		//this.doubleValue = doubleValue;
+	//}
+	
+	public T pop(){ //T pop() //generic datatype
+		
+	}
+	
+	public void push(T value){ //void push() //no datatype
+		
+	}
+	
+	public boolean isEmpty(){ //boolean isEmpty()
+		
+	}
+	
+
+	public double getMax(){ //double getMax() return as double, so we must cast
+		 
+	}
+	
+}
+
+
+
+
